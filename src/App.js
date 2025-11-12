137bdb592a1d0f307bd137af6133c4f6b885db12
OneCoffeeOneIdea <47437965+OneIdeaOneCoffee@users.noreply.github.com>
2025-11-11 21:29:36 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -161,7 +161,7 @@ class IRCClient {
   this.emit("error", { type: err.type });
 });
   
-  startHeartbeat(); {
+  startHeartbeat() {
     this.heartbeatInterval = setInterval(() => {
       if (this.ws && this.ws.readyState === WebSocket.OPEN) {
         const now = Date.now();
@@ -172,7 +172,7 @@ class IRCClient {
     }, 20000);
   }
   
-  stopHeartbeat(); {
+  stopHeartbeat() {
     if (this.heartbeatInterval) {
       clearInterval(this.heartbeatInterval);
       this.heartbeatInterval = null;
